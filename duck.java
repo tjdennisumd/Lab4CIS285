bf134bfa7729836acbc871c35f0cddd4fd7223ab
Tim Dennison <tjdennis@umich.edu>
2019-10-10 16:17:11 -0400

    Duck.java john branch

---
+++ duck.java
@@ -0,0 +1,15 @@
+public class duck
+{
+  public fly()
+  {
+    system.out.println("A duck can fly");
+  }
+  public swim()
+  {
+    system.out.println("A duck can swim");
+  }
+  public eat()
+  {
+    system.out.println("A duck can eat);
+  }
+}
